3f814f66eb0550aa3670f2a487843ed9c9027ef4
gayantha <gayantha@bluelotus360.com>
2025-03-12 13:57:07 +0530

    mmmmmmmm wow

---
+++ src/app/base64-images.spec.ts
@@ -0,0 +1,7 @@
+import { BASE64Images } from './base64-images';
+
+describe('BASE64Images', () => {
+  it('should create an instance', () => {
+    expect(new BASE64Images()).toBeTruthy();
+  });
+});
